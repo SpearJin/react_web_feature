32764633dd2db5e1605ca8ceddc3c16e4c10e481
SpearJin <ckdwls2525@gmail.com>
2022-01-28 20:57:13 +0900

    modal 기능 구현

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './modal.css';
 
-const Modal = (props) => (
-  <div className='modal'>
-    <button className='modal_btn'>
-      <span className='modal_btn_text'>Open Modal</span>
-    </button>
-    <div className='modal_window'>
-      <div className='modal_window_container'>
-        <span className='modal_window_btn'>x</span>
-        <span className='modal_window_text'>HELLO CODESTATES!</span>
+const Modal = (props) => {
+  const modal = useRef(null);
+
+  const onClickModal = () => {
+    modal.current.classList.add('active');
+  };
+
+  const onClickCancle = (e) => {
+    const target = e.target;
+    if (!target.className.includes('cancle')) {
+      return;
+    }
+    modal.current.classList.remove('active');
+  };
+
+  return (
+    <div className='modal'>
+      <button className='modal_btn' onClick={onClickModal}>
+        <span className='modal_btn_text'>Open Modal</span>
+      </button>
+      <div className='modal_window' ref={modal}>
+        <div className='modal_window_container' onClick={onClickCancle}>
+          <span className='modal_window_btn cancle'>x</span>
+          <span className='modal_window_text'>HELLO CODESTATES!</span>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Modal;
