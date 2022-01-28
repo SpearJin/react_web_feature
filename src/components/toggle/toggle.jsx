80ca3491dbb251087e055501d561797970e04e78
SpearJin <ckdwls2525@gmail.com>
2022-01-28 20:18:27 +0900

    checkBox 마크업 & 스타일링

--- src/components/toggle/toggle.jsx
+++ src/components/toggle/toggle.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import './toggle.css';
 
-const Toggle = (props) => <div>토글</div>;
+const Toggle = () => {
+  return (
+    <div className='toggle'>
+      <label className='toggle_label'>
+        <input type='checkbox' className='toggle_input' />
+        <span className='toggle_span'></span>
+      </label>
+    </div>
+  );
+};
 
 export default Toggle;
