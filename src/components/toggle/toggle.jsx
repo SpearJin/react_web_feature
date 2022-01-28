c9f3433ce1be0af3f345fcf7105a4a35096cda42
SpearJin <ckdwls2525@gmail.com>
2022-01-28 20:25:19 +0900

    checkBox 기능 구현

--- src/components/toggle/toggle.jsx
+++ src/components/toggle/toggle.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './toggle.css';
 
 const Toggle = () => {
+  const [switchText, setSwitchText] = useState('OFF');
+
+  const onClickSwitch = (e) => {
+    const checked = e.target.checked;
+    if (checked) {
+      setSwitchText('ON');
+    } else {
+      setSwitchText('OFF');
+    }
+  };
+
   return (
     <div className='toggle'>
       <label className='toggle_label'>
-        <input type='checkbox' className='toggle_input' />
+        <input type='checkbox' className='toggle_input' onClick={onClickSwitch} />
         <span className='toggle_span'></span>
       </label>
+      <p>Toggle Switch {switchText}</p>
     </div>
   );
 };
