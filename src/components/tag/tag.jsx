304ae3e3d67141c194bbefea0e6083bc74563186
SpearJin <ckdwls2525@gmail.com>
2022-01-31 15:40:58 +0900

    tag 기능 구현

--- src/components/tag/tag.jsx
+++ src/components/tag/tag.jsx
@@ -1,22 +1,39 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './tag.css';
 
 const Tag = (props) => {
-  const [tagList, setTagList] = useState(['list1', 'list2']);
+  const [tagList, setTagList] = useState(['CodeStates', 'JJang']);
+  const tagItem = useRef(null);
+
+  const onSubmitTagAdd = (e) => {
+    e.preventDefault();
+    const tags = [...tagList, tagItem.current.value];
+    setTagList(tags);
+    tagItem.current.value = '';
+  };
+
+  const onClickTagRemove = (e) => {
+    console.log(e.target);
+    const target = e.target.closest('.tag_item');
+    target.remove();
+  };
 
   return (
     <div className='tag'>
       <h1 className='title'>Tag</h1>
-      <div className='tag_container'>
+      <form className='tag_container' onSubmit={onSubmitTagAdd}>
         <ul className='tag_list'>
-          {tagList.map((tag) => (
-            <li className='tag_item'>
-              {tag} <span className='tag_item-delete'>x</span>
+          {tagList.map((tag, i) => (
+            <li className='tag_item' key={i}>
+              <span className='tag_item-text'>{tag}</span>
+              <span className='tag_item-delete' onClick={onClickTagRemove}>
+                x
+              </span>
             </li>
           ))}
-          <input className='tag_input' type='text' placeholder='Press enter to add tags'></input>
+          <input className='tag_input' ref={tagItem} type='text' placeholder='Press enter to add tags' />
         </ul>
-      </div>
+      </form>
     </div>
   );
 };
